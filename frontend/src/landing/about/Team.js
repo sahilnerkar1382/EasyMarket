28d8cadcc2b31277954ac30f8a5d04ffcbf2e38d
Sahil Nerkar <sahilnerkar0@gmail.com>
2025-09-29 23:36:34 +0530

    about e  section completed

--- frontend/src/landing/about/Team.js
+++ frontend/src/landing/about/Team.js
@@ -13,7 +13,7 @@ function Team() {
       >
         <div className="col-6 p-3 text-center">
           <img
-            src="media/images/nithinKamath.jpg"
+            src="images/nithinKamath.jpg"
             style={{ borderRadius: "100%", width: "50%" }}
           />
           <h4 className="mt-5">Nithin Kamath</h4>
